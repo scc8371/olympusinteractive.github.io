b664af9842bb640adc9efd334b61c7f6f1c5535c
Sami Chamberlain <70332182+scc8371@users.noreply.github.com>
2024-07-08 13:46:02 -0400

    fix hero scaling for image

--- src/assets.ts
+++ src/assets.ts
@@ -1,6 +1,6 @@
 import logo from './assets/logos/olympus.png'
 import logo_no_background from './assets/logos/logo_no_background.png'
-import logo_pantheon from "./assets/logos/pantheon.svg"
+import logo_pantheon from "./assets/logos/pantheon.webp"
 
 export default {
     "logo" : logo,
